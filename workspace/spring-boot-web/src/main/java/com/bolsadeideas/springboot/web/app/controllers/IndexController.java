4bf5dbf75d655ab7f40e169a9d28f4cb60f92048
dimekaimorts <kaimorts817@gmail.com>
2022-10-19 15:24:14 -0500

    update(handler): Un nuevo handler ha sido creado para la vista Perfil

--- workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -5,14 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
 @Controller
 @RequestMapping(value = "/app")
 public class IndexController {
 
     @GetMapping(value = {"","/index", "/", "/home"})
-    public String index(Model mv) {
-        mv.addAttribute("titulo", "Hola Spring Boot Framework con Model");
+    public String index(Model model) {
+        model.addAttribute("titulo", "Hola Spring Boot Framework con Model");
         return "index";
+    }
+
+    @RequestMapping(value = "/perfil")
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("Andres");
+        usuario.setApellido("Gonzalez");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+        return "perfil";
     }
 
 }
