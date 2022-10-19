016e2ad22afef9aaff8a8196fbb3b3beeed969a8
dimekaimorts <kaimorts817@gmail.com>
2022-10-19 18:34:54 -0500

    update(model): Se agrega el atributo email

--- workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
+++ workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
@@ -3,6 +3,16 @@ package com.bolsadeideas.springboot.web.app.models;
 public class Usuario {
     private String nombre;
     private String apellido;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
 
     public String getNombre() {
         return nombre;
@@ -18,6 +28,14 @@ public class Usuario {
 
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
 }
